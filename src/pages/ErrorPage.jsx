c34d8eeddadc4adf08d38fb2e2e6b108a6bfbeef
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2025-01-18 13:58:23 +0600

    Add 404 Page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,21 @@
 import { Helmet } from "react-helmet-async";
+import { useNavigate } from "react-router-dom";
 
 export default function ErrorPage() {
+  const navigate = useNavigate();
   return (
     <>
       <Helmet>
         <title>404 - Page Not Found</title>
       </Helmet>
-      ErrorPage
+      <section className="flex h-screen items-center justify-center">
+        <img
+          onClick={() => navigate("/")}
+          className="w-32 cursor-pointer sm:w-36 md:w-40 lg:w-48"
+          src="../../src/assets/page-not-found.svg"
+          alt="Page Not Found"
+        />
+      </section>
     </>
   );
 }
