6e9e213403f58232740c7b4aa6399e9199edf27d
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2025-01-24 19:16:00 +0600

    Add Statistics.jsx

--- src/pages/Dashboard/Admin/Statistics.jsx
+++ src/pages/Dashboard/Admin/Statistics.jsx
@@ -1,3 +1,47 @@
+import { PieChart, Pie, Cell, Tooltip } from "recharts";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Statistics() {
-  return <div>Statistics</div>;
+  const axiosSecure = useAxiosSecure();
+
+  const { data: statistics = [] } = useQuery({
+    queryKey: ["statistics"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/statistics`);
+      return data;
+    },
+  });
+  return (
+    <>
+      <section className="flex h-[calc(100vh-96px)] items-center justify-center">
+        <div className="">
+          <div className="overflow-x-auto">
+            <PieChart width={240} height={240}>
+              <Pie
+                data={statistics}
+                cx={115}
+                cy={115}
+                labelLine={false}
+                label="value"
+                outerRadius={80}
+                fill="#000000"
+                dataKey="value"
+              >
+                {statistics.map((entry, index) => (
+                  <Cell key={`cell-${index}`} />
+                ))}
+              </Pie>
+              <Tooltip />
+            </PieChart>
+          </div>
+          <div>
+            <h1 className="mt-4 text-center text-xl font-semibold">
+              Statistics
+            </h1>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
